3541483bef4556b604b22577d8cc6ad970afd885
Mitchell May <7408476+mjmay08@users.noreply.github.com>
2023-08-07 12:01:29 -0500

    Add webtorrent related packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import  path from 'path';
 import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -30,6 +31,7 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/client/index.html'
-    })
+    }),
+    new NodePolyfillPlugin({})
   ]
 }
